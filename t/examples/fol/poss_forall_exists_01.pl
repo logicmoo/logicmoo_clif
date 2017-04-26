a3aa508277ad0565ce3bdc1a7f4b82077b400762
root <logicmoo@gmail.com>
2017-04-26 01:41:52 -0700

    test 01  is getting complete 2017-04-26-01:41:38

--- t/examples/fol/poss_forall_exists_01.pl
+++ t/examples/fol/poss_forall_exists_01.pl
@@ -1,8 +1,29 @@
 #!/usr/bin/env swipl
 
-:- ensure_loaded(logicmoo_user).
+% =================================================================================
+% Load the system
+% =================================================================================
+
+:- ensure_loaded(library(logicmoo_user)).
+
 :- set_prolog_flag(dialect,clif).
 
+% =================================================================================
+% Set our engine up
+% =================================================================================
+
+% deduce instances from usages in args having the effect of deducing human,dwelling,beverage_class are classes
+==> make_wff(true).
+% set false so make_wff/1 will be noticed (default is true)
+==> assume_wff(false).
+% set truth maintainance system to remove previous assertions that new assertions disagree with 
+==> tms_mode(remove_conflicting).
+
+:- set_prolog_flag(runtime_debug,3). % mention it when we remove previous assertions
+
+% =================================================================================
+% Define a couple predicates
+% =================================================================================
 
 % maximum cardinality of livesAt/2 is 1
 instance(livesAt,'FunctionalBinaryPredicate').
@@ -11,17 +32,21 @@ arity(livesAt,2).
 domain(livesAt,1,human).
 domain(livesAt,2,dwelling).
 
-% maximum cardinality of livesAt/2 is 1
+% define drinks/2
 arity(drinks,2).
 domain(drinks,1,human).
 domain(drinks,2,beverage_class).
 
+% =================================================================================
+% Note these two assertions are implicit to the system and have no side effect 
+% (they are here to serve as a reminder)
+% =================================================================================
 
-% all objects in the universe that may drink coffee do drink coffee
-all(X, if(possible(drinks(X, coffee)),drinks(X, coffee))).
+% all objects in the universe that do drink coffee, may drink coffee
+all(X, if(drinks(X, coffee),possible(drinks(X, coffee)))).
 
-% all objects in the universe that may live in the green house do live in the green house
-all(X, if(possible(livesAt(X, green)),lives(X, green) )).
+% for any objects in the universe that live in the green house must obvously have that as a possibility
+all(X, if(livesAt(X, green),possible(livesAt(X, green)))).
 
 % =================================================================================
 % But given the above: 
@@ -29,8 +54,27 @@ all(X, if(possible(livesAt(X, green)),lives(X, green) )).
 %   Only things that possibly can drink coffee live in the green house?
 %
 % =================================================================================
-all(X, lives(X, green) & drinks(X, coffee)).
+all(X, livesAt(X, green_house) & drinks(X, coffee)).
+
+~possible(livesAt(fred,green_house)).
+
+% Does fred drink coffee? (should be unknown)
+:- \+ drinks(fred,coffee).
+
+possible(livesAt(joe,green_house)).
+:- drinks(joe,coffee).
 
 
-% this is just an asserion test..  it cannont fail (it is used to set up other tests)
+% =================================================================================
+% These two assertions are a bit weird but are for fun
+% =================================================================================
+
+% all objects in the universe that may drink coffee do drink coffee
+all(X, if(possible(drinks(X, coffee)),drinks(X, coffee))).
+
+% all objects in the universe that may live in the green house do live in the green house
+all(X, if(possible(livesAt(X, green_house)),lives(X, green_house) )).
+
+
+
 
