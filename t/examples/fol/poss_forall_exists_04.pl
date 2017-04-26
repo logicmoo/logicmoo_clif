b74249b96a49dcd526753a638e82e5625645c6f5
root <logicmoo@gmail.com>
2017-04-26 01:09:29 -0700

    updated comments? 2017-04-26-01:09:16

--- t/examples/fol/poss_forall_exists_04.pl
+++ t/examples/fol/poss_forall_exists_04.pl
@@ -3,12 +3,59 @@
 :- ensure_loaded(logicmoo_user).
 :- set_prolog_flag(dialect,clif).
 
-% all objects in the universe that may drink coffee do drink coffee
-all(X, if(possible(drinks(X, coffee)),drinks(X, coffee))).
 
-% all objects in the universe that may live in the green house do live in the green house
-all(X, if(possible(lives(X, green)),lives(X, green) )).
+% maximum cardinality of livesAt/2 is 1
+instance(livesAt,'FunctionalBinaryPredicate').
+% thus implies
+arity(livesAt,2).
+domain(livesAt,1,human).
+domain(livesAt,2,dwelling).
 
-% only things that possibly can drink coffee live in the green house?
-all(X, lives(X, green) & drinks(X, coffee)).
+% maximum cardinality of livesAt/2 is 1
+arity(drinks,2).
+domain(drinks,1,human).
+domain(drinks,2,beverage_class).
+
+
+
+% =================================================================================
+% Note these two assertions are implicit to the system and have no side effect
+% =================================================================================
+
+% all objects in the universe that do drink coffee, may drink coffee
+all(X, if(drinks(X, coffee),possible(drinks(X, coffee)))).
+
+% for any objects in the universe that live in the green house must obvously have that as a possibility
+all(X, if(livesAt(X, green),possible(livesAt(X, green)))).
+
+% =================================================================================
+% Some facts about the world
+% =================================================================================
+
+livesAt(joe,red_house).
+
+:- must(~possible(livesAt(joe,green_house))).
+
+:- printAll(mpred_why(~possible(livesAt(joe,green_house)),_Why)).
+
+~possible(drinks(bob,coffee)).
+
+~possible(livesAt(fred,green_house)).
+
+% =================================================================================
+% But given the above: 
+%
+%   Only things that possibly can drink coffee live in the green house?
+%  
+%   Only currently individuals whom are not living in the red house live in the green?
+%
+% =================================================================================
+exists(X, livesAt(X, green_house) & drinks(X, coffee)).
+
+% Does anyone live at the green house? (Should be one right?)
+:- mpred_test(livesAt(_X,green_house)).
+
+% Can anyone live at the green house? (Should be everyone but the one listed above?)
+:- mpred_test(~possible(livesAt(_X,green_house))).
+
 
