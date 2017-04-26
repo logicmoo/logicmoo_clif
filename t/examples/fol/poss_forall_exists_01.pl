4e230154564b5c99a39710c4c99d5395635a495e
root <logicmoo@gmail.com>
2017-04-26 01:12:17 -0700

    updated comments? 2017-04-26-01:12:04

--- t/examples/fol/poss_forall_exists_01.pl
+++ t/examples/fol/poss_forall_exists_01.pl
@@ -3,13 +3,32 @@
 :- ensure_loaded(logicmoo_user).
 :- set_prolog_flag(dialect,clif).
 
+
+% maximum cardinality of livesAt/2 is 1
+instance(livesAt,'FunctionalBinaryPredicate').
+% thus implies
+arity(livesAt,2).
+domain(livesAt,1,human).
+domain(livesAt,2,dwelling).
+
+% maximum cardinality of livesAt/2 is 1
+arity(drinks,2).
+domain(drinks,1,human).
+domain(drinks,2,beverage_class).
+
+
 % all objects in the universe that may drink coffee do drink coffee
 all(X, if(possible(drinks(X, coffee)),drinks(X, coffee))).
 
 % all objects in the universe that may live in the green house do live in the green house
-all(X, if(possible(lives(X, green)),lives(X, green) )).
+all(X, if(possible(livesAt(X, green)),lives(X, green) )).
 
-% only things that possibly can drink coffee live in the green house?
+% =================================================================================
+% But given the above: 
+%
+%   Only things that possibly can drink coffee live in the green house?
+%
+% =================================================================================
 all(X, lives(X, green) & drinks(X, coffee)).
 
 
