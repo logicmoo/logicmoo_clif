636ef299abfad72e0c38d983fcf418acfd6777c4
Douglas R. Miles <logicmoo@gmail.com>
2018-04-06 10:17:26 -0700

    needed simpler setup

--- t/examples/fol/room_numbers_01.pfc.pl
+++ t/examples/fol/room_numbers_01.pfc.pl
@@ -4,25 +4,21 @@
 :- include(test_header).
 :- set_kif_option(+assert).
 
+% Version A - Simpler
 exists(R1,room_number(R1,22)).
-
 exists(R1,room_number(R1,77)).
-
-all(R, (room_number(R,22) => (room(R) & ~big(R)))) .    
-
+all(R, (room_number(R,22) => (room(R) & ~big(R)))) .
 all(R, (room_number(R,77) => (room(R) & big(R)))) .    
 
 % need proof that 
-?- sanity_test(( room_number(R1,22) & room_number(R2,77) => R1 \= R2 )).
+?- must_be_satifiable(( room_number(R1,22) & room_number(R2,77) => R1 \= R2 )).
 
 
-end_of_file.
+:- reset_kb(kbii).
 
-% simpler
+% Version B - Simpler
 all(R, (room_number(R,22) => ( ~big(R)))) .    
-all(R, (room_number(R,77) => ( big(R)))) .    
+all(R, (room_number(R,77) => ( big(R)))) .
+
 % need proof that 
-?- sanity_test(( room_number(R1,22) & room_number(R2,77) => R1 \= R2 )).
-
-
-
+?- must_be_satifiable(( room_number(R1,22) & room_number(R2,77) => R1 \= R2 )).
